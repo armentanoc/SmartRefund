e1d21eeecf5c2b7cc4ffa6166a79a251e02f8255
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-07 16:33:58 -0300

    feat: injetando logger no service
    Co-authored-by: czambanini <ca.zambanini@gmail.com>
    Co-authored-by: lauradefaria <lauradfma@gmail.com>
    Co-authored-by: ig-nunes <inunes.us@gmail.com>
    Co-authored-by: paulaandrezza <paulaandrezza25@gmail.com>
    Co-authored-by: cristopherkovalski <cristopherkovalski@gmail.com>

--- src/SmartRefund.Application/Services/FileValidatorService.cs
+++ src/SmartRefund.Application/Services/FileValidatorService.cs
@@ -1,4 +1,5 @@
-﻿using SmartRefund.Application.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using SmartRefund.Application.Interfaces;
 using SmartRefund.Infra.Interfaces;
 
 namespace SmartRefund.Application.Services
@@ -6,9 +7,11 @@ namespace SmartRefund.Application.Services
     public class FileValidatorService : IFileValidatorService
     {
         private IRepositoryTeste _repository;
-        public FileValidatorService(IRepositoryTeste repository)
+        private ILogger<FileValidatorService> _logger;
+        public FileValidatorService(IRepositoryTeste repository, ILogger<FileValidatorService> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
     }
 }
