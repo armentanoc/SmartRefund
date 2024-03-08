d6eb839d1f1cf1924fd43755e943ca2ecb5abbc1
Igor Nunes <inunes.us@gmail.com>
2024-03-08 17:39:29 -0300

    feat: add InternalAnalyzerService and ManagementController

---
+++ src/SmartRefund.Application/Services/InternalAnalyzerService.cs
@@ -0,0 +1,20 @@
+﻿using SmartRefund.Infra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRefund.Application.Services
+{
+    public class InternalAnalyzerService
+    {
+        private readonly ITranslatedVisionReceiptRepository _receiptRepository;
+
+        public InternalAnalyzerService(ITranslatedVisionReceiptRepository receiptRepository)
+        {
+            _receiptRepository = receiptRepository;
+        }
+
+    }
+}
---
+++ src/SmartRefund.WebAPI/Controllers/ManagementController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartRefund.Infra.Interfaces;
+
+namespace SmartRefund.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ManagementController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
