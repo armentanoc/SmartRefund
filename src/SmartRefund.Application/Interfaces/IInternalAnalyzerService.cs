b91e5d73f0f64091a59aa7c5fae3257417e03f7b
Igor Nunes <inunes.us@gmail.com>
2024-03-10 14:26:06 -0300

    feat: add IInternalAnalyzerService interface

---
+++ src/SmartRefund.Application/Interfaces/IInternalAnalyzerService.cs
@@ -0,0 +1,14 @@
+﻿using SmartRefund.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRefund.Application.Interfaces
+{
+    public interface IInternalAnalyzerService
+    {
+        Task<TranslatedVisionReceipt> UpdateStatus(uint id, string newStatus);
+    }
+}
--- src/SmartRefund.Application/Services/InternalAnalyzerService.cs
+++ src/SmartRefund.Application/Services/InternalAnalyzerService.cs
@@ -1,4 +1,8 @@
-﻿using SmartRefund.Infra.Interfaces;
+﻿using SmartRefund.Application.Interfaces;
+using SmartRefund.CustomExceptions;
+using SmartRefund.Domain.Enums;
+using SmartRefund.Domain.Models;
+using SmartRefund.Infra.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +11,34 @@ using System.Threading.Tasks;
 
 namespace SmartRefund.Application.Services
 {
-    public class InternalAnalyzerService
+    public class InternalAnalyzerService : IInternalAnalyzerService
     {
-        private readonly ITranslatedVisionReceiptRepository _receiptRepository;
+        private readonly ITranslatedVisionReceiptRepository _translatedVisionReceiptRepository;
 
-        public InternalAnalyzerService(ITranslatedVisionReceiptRepository receiptRepository)
+        public InternalAnalyzerService(ITranslatedVisionReceiptRepository translatedVisionReceiptRepository)
         {
-            _receiptRepository = receiptRepository;
+            _translatedVisionReceiptRepository = translatedVisionReceiptRepository;
+        }
+
+        public async Task<TranslatedVisionReceipt> UpdateStatus(uint id, string newStatus)
+        {
+            var translatedVisionReceipt = await GetById(id);
+
+            if (Enum.TryParse<TranslatedVisionReceiptStatusEnum>(newStatus, true, out var result))
+            {
+                translatedVisionReceipt.SetStatus(result);
+                return await _translatedVisionReceiptRepository.UpdateAsync(translatedVisionReceipt);
+            }
+
+            throw new InvalidOperationException();
+
+        }
+
+        private async Task<TranslatedVisionReceipt> GetById(uint id)
+        {
+            var translatedVisionReceipt = await _translatedVisionReceiptRepository.GetAsync(id);
+
+            return translatedVisionReceipt;
         }
 
     }
