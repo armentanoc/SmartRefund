335711ab43399f09298a6354b215bf85c7f24db1
Igor Nunes <inunes.us@gmail.com>
2024-03-10 14:27:25 -0300

    feat: add UpdateTVRStatusRequest class

---
+++ src/SmartRefund.ViewModels/Requests/UpdateTVRStatusRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRefund.ViewModels.Requests
+{
+    public class UpdateTVRStatusRequest
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public uint Id { get; init; }
+
+        [Required(ErrorMessage = "New status is required.")]
+        public string NewStatus { get; init; }
+
+        public UpdateTVRStatusRequest(string newStatus, uint id)
+        {
+            NewStatus = newStatus;
+            Id = id;
+        }
+    }
+}
