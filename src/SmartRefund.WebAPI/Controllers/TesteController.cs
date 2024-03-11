6cc42a9f3893275141599daa3576ed36cf59cabf
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-11 12:44:28 -0300

    refactor: working db context and dependency inj Co-authored-by: paulaandrezza <paulaandrezza25@gmail.com>

---
+++ src/SmartRefund.WebAPI/Controllers/TesteController.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SmartRefund.Application.Interfaces;
+using SmartRefund.Domain.Models;
+using SmartRefund.Infra.Interfaces;
+using SmartRefund.ViewModels.Requests;
+
+namespace SmartRefund.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesteController : ControllerBase
+    {
+        public IRawVisionReceiptRepository _repository;
+        public ILogger<TesteController> _logger;
+        public IVisionTranslatorService _service;
+        public TesteController(IRawVisionReceiptRepository repository, ILogger<TesteController> logger, IVisionTranslatorService service)
+        {
+            _repository = repository;   
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RawVisionReceiptViewModelTeste rawVisionReceiptTeste)
+        {
+            var internalReceipt = new InternalReceipt(rawVisionReceiptTeste.InternalReceiptId);
+            var rawVisionReceipt = new RawVisionReceipt(internalReceipt, rawVisionReceiptTeste.IsReceipt, rawVisionReceiptTeste.Category, rawVisionReceiptTeste.Total, rawVisionReceiptTeste.Description, rawVisionReceiptTeste.IsTranslated);
+            var createdRawVisionReceipt = await _repository.AddAsync(rawVisionReceipt);
+            return CreatedAtAction("GetItemById", new { id = createdRawVisionReceipt.Id }, new { message = "Resource created successfully.", data = createdRawVisionReceipt });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<RawVisionReceipt> GetItemById([FromRoute] uint id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        [HttpGet("testaTraducao")]
+        public async Task<ActionResult<IEnumerable<TranslatedVisionReceipt>>> TryTestingTranslation()
+        {
+            IEnumerable<RawVisionReceipt> rawVisionReceipts = await _repository.GetAllAsync();
+            IEnumerable<TranslatedVisionReceipt> translatedReceipts = new List<TranslatedVisionReceipt>();
+
+            foreach (var rawReceipt in rawVisionReceipts)
+            {
+                var translatedReceipt = _service.GetTranslatedVisionReceipt(rawReceipt);
+                translatedReceipts.Append(translatedReceipt);
+            }
+
+            return Ok(translatedReceipts);
+        }
+    }
+}
