080e5bb653c6dec1c73f22827c34ae0c18e8ce40
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-11 15:40:21 -0300

    feat: domain adjustments
    Co-authored-by: paulaandrezza <paulaandrezza25@gmail.com>

---
+++ src/SmartRefund.Domain/Models/InternalReceipt.cs
@@ -0,0 +1,31 @@
+﻿using SmartRefund.Domain.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRefund.Domain.Models
+{
+    public class InternalReceipt : BaseEntity
+    {
+        public uint EmployeeId { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public InternalReceiptStatusEnum Status { get; private set; }
+
+        //Recebe a imagem
+
+        public InternalReceipt(uint employeeId)
+        {
+            CreationDate = DateTime.Now;
+            EmployeeId = employeeId;
+            Status = InternalReceiptStatusEnum.Unprocessed;
+        }
+        public InternalReceipt() { }
+
+        public void SetStatus(InternalReceiptStatusEnum status)
+        {
+            Status = status;
+        }
+    }
+}
--- src/SmartRefund.Domain/Models/RawVisionReceipt.cs
+++ src/SmartRefund.Domain/Models/RawVisionReceipt.cs
@@ -1,9 +1,29 @@
-﻿namespace SmartRefund.Domain.Models
+﻿using SmartRefund.Domain.Enums;
+
+namespace SmartRefund.Domain.Models
 {
     public class RawVisionReceipt : BaseEntity
     {
+
+        public InternalReceipt InternalReceipt { get; private set; }
+        public string IsReceipt { get; private set; }
+        public string Category { get; private set; }
+        public string Total { get; private set; }
+        public string Description { get; private set; }
+        public bool IsTranslated { get; private set; }
+
         public RawVisionReceipt()
         {
+        }
+
+        public RawVisionReceipt(InternalReceipt internalReceipt, string isReceipt, string category, string total, string description, bool isTranslated)
+        {
+            InternalReceipt = internalReceipt;
+            IsReceipt = isReceipt;
+            Category = category;
+            Total = total;
+            Description = description;
+            IsTranslated = isTranslated;
         }
     }
 }
