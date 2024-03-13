52883bc2c06790efe132a704bc0acf0a23e97461
armentanoc <armentanocarolina@gmail.com>
2024-03-13 13:36:02 -0300

    refactor: organizing prompt configuration.

---
+++ src/SmartRefund.Application/Interfaces/IVisionExecutorServiceConfiguration.cs
@@ -0,0 +1,60 @@
+﻿namespace SmartRefund.Application.Interfaces
+{
+    public interface IVisionExecutorServiceConfiguration
+    {
+        string OpenAIVisionApiEnvVar { get; }
+        OpenAIVisionPrompts OpenAIVisionPrompts { get; }
+    }
+
+    public class OpenAIVisionPrompts
+    {
+        private string? _systemPrompt;
+        public string SystemPrompt
+        {
+            get => _systemPrompt;
+            set => _systemPrompt = ValidatePrompt(value, "System prompt");
+        }
+
+        private string? _imagePrompt;
+        public string ImagePrompt
+        {
+            get => _imagePrompt;
+            set => _imagePrompt = ValidatePrompt(value, "Image prompt");
+        }
+
+        private string? _isReceiptPrompt;
+        public string IsReceiptPrompt
+        {
+            get => _isReceiptPrompt;
+            set => _isReceiptPrompt = ValidatePrompt(value, "IsReceipt prompt");
+        }
+
+        private string? _totalPrompt;
+        public string TotalPrompt
+        {
+            get => _totalPrompt;
+            set => _totalPrompt = ValidatePrompt(value, "Total prompt");
+        }
+
+        private string? _categoryPrompt;
+        public string CategoryPrompt
+        {
+            get => _categoryPrompt;
+            set => _categoryPrompt = ValidatePrompt(value, "Category prompt");
+        }
+
+        private string? _descriptionPrompt;
+        public string DescriptionPrompt
+        {
+            get => _descriptionPrompt;
+            set => _descriptionPrompt = ValidatePrompt(value, "Description prompt");
+        }
+
+        private string ValidatePrompt(string value, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException($"Configuration of prompt {propertyName} cannot be null or whitespace");
+            return value;
+        }
+    }
+}
---
+++ src/SmartRefund.Application/Services/VisionExecutorServiceConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using SmartRefund.Application.Interfaces;
+using SmartRefund.CustomExceptions;
+
+namespace SmartRefund.Application.Services
+{
+    public class VisionExecutorServiceConfiguration : IVisionExecutorServiceConfiguration
+    {
+        public string OpenAIVisionApiEnvVar
+        {
+            get
+            {
+                var apiKey = _configuration["OpenAIVisionConfig:EnvVariable"];
+                if (string.IsNullOrWhiteSpace(apiKey))
+                    throw new ApiKeyNotFoundException();
+                return apiKey;
+            }
+        }
+
+        private readonly IConfiguration _configuration;
+
+        public VisionExecutorServiceConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public OpenAIVisionPrompts OpenAIVisionPrompts => new OpenAIVisionPrompts
+        {
+            SystemPrompt = _configuration["OpenAIVisionConfig:Prompts:System"],
+            ImagePrompt = _configuration["OpenAIVisionConfig:Prompts:User:Image"],
+            IsReceiptPrompt = _configuration["OpenAIVisionConfig:Prompts:User:IsReceipt"],
+            TotalPrompt = _configuration["OpenAIVisionConfig:Prompts:User:Total"],
+            CategoryPrompt = _configuration["OpenAIVisionConfig:Prompts:User:Category"],
+            DescriptionPrompt = _configuration["OpenAIVisionConfig:Prompts:User:Description"]
+        };
+    }
+}
