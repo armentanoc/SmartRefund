b8682ae7898f93a13237b4b3a46b7364052a4a9c
armentanoc <armentanocarolina@gmail.com>
2024-03-13 13:36:18 -0300

    refactor: vision executor service.

--- src/SmartRefund.Application/Interfaces/IVisionExecutorService.cs
+++ src/SmartRefund.Application/Interfaces/IVisionExecutorService.cs
@@ -1,14 +1,20 @@
-﻿
-using OpenAI_API;
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
 using SmartRefund.Domain.Models;
 using SmartRefund.Domain.Models.Enums;
+using SmartRefund.ViewModels.Responses;
 
 namespace SmartRefund.Application.Interfaces
 {
     public interface IVisionExecutorService
     {
+        Task<RawVisionReceipt> ExecuteRequestAsync(InternalReceipt input);
+        Task<RawVisionResponse> ProcessVisionResponseAsync(Conversation conversation, byte[] rawImage, InternalReceipt receipt);
+        Task<RawVisionReceipt> CreateRawVisionReceiptAsync(InternalReceipt receipt, RawVisionResponse response);
+        Task<InternalReceipt> UpdateInternalReceiptAsync(InternalReceipt input);
+        Task<string> GetResponseAsync(Conversation conversation, string prompt, Exception exceptionIfInvalid);
         OpenAIAPI ConfigureApiKey();
         bool IsExecutableStatus(InternalReceiptStatusEnum status);
-        Task<RawVisionReceipt> ExecuteRequest(InternalReceipt internalReceipt);
+        bool IsInvalidAnswer(string answer);
     }
 }
--- src/SmartRefund.Application/Services/VisionExecutorService.cs
+++ src/SmartRefund.Application/Services/VisionExecutorService.cs
@@ -1,97 +1,115 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using SmartRefund.Infra.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using SmartRefund.Application.Interfaces;
+using SmartRefund.CustomExceptions;
 using SmartRefund.Domain.Models.Enums;
 using SmartRefund.Domain.Models;
+using SmartRefund.Infra.Interfaces;
+using SmartRefund.ViewModels.Responses;
+using System.Text.RegularExpressions;
 using OpenAI_API.Models;
 using OpenAI_API.Chat;
 using OpenAI_API;
-using SmartRefund.CustomExceptions;
 
-namespace SmartRefund.Application.Services
+public class VisionExecutorService : IVisionExecutorService
 {
-    public class VisionExecutorService : IVisionExecutorService
+    private IInternalReceiptRepository _internalReceiptRepository;
+    private IRawVisionReceiptRepository _rawVisionReceiptRepository;
+    private ILogger<VisionExecutorService> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly IVisionExecutorServiceConfiguration _visionConfig;
+
+    public VisionExecutorService(
+        IInternalReceiptRepository repository,
+        IRawVisionReceiptRepository rawVisionReceiptRepository,
+        ILogger<VisionExecutorService> logger,
+        IConfiguration configuration,
+        IVisionExecutorServiceConfiguration visionConfig)
     {
-        private IInternalReceiptRepository _internalReceiptRepository;
-        private IRawVisionReceiptRepository _rawVisionReceiptRepository;
-        private ILogger<VisionExecutorService> _logger;
-        private IConfiguration _configuration;
+        _internalReceiptRepository = repository;
+        _rawVisionReceiptRepository = rawVisionReceiptRepository;
+        _logger = logger;
+        _configuration = configuration;
+        _visionConfig = visionConfig;
+    }
 
-        public VisionExecutorService(IInternalReceiptRepository repository, IRawVisionReceiptRepository rawVisionReceiptRepository, ILogger<VisionExecutorService> logger, IConfiguration configuration)
-        {
-            _internalReceiptRepository = repository;
-            _rawVisionReceiptRepository = rawVisionReceiptRepository;
-            _logger = logger;
-            _configuration = configuration;
-        }
+    public async Task<RawVisionReceipt> ExecuteRequestAsync(InternalReceipt input)
+    {
+        if (!IsExecutableStatus(input.Status))
+            throw new NonVisionExecutableStatus(input.Id, input.Status.ToString());
 
-        public async Task<RawVisionReceipt> ExecuteRequest(InternalReceipt input)
-        {
-            if (!IsExecutableStatus(input.Status))
-                throw new NonVisionExecutableStatus(input.Id, input.Status.ToString()); 
+        OpenAIAPI api = ConfigureApiKey();
+        var rawImage = input.Image;
+        var conversation = api.Chat.CreateConversation();
+        conversation.Model = Model.GPT4_Vision;
+        var response = await ProcessVisionResponseAsync(conversation, rawImage, input);
+        var addedRawVisionReceipt = await CreateRawVisionReceiptAsync(input, response);
+        await UpdateInternalReceiptAsync(input);
 
-            OpenAIAPI api = ConfigureApiKey();
-            var rawImage = input.Image;
-            var chat = api.Chat.CreateConversation();
-            chat.Model = Model.GPT4_Vision;
+        return addedRawVisionReceipt;
+    }
 
-            chat.AppendSystemMessage("Você é um especialista em ler notas fiscais e extrair informações importantes.");
+    public async Task<RawVisionResponse> ProcessVisionResponseAsync(Conversation conversation, byte[] rawImage, InternalReceipt input)
+    {
+        var response = new RawVisionResponse();
+        var prompts = _visionConfig.OpenAIVisionPrompts;
 
-            chat.AppendUserInput("Você deve considerar essa imagem de nota fiscal para responder às próximas perguntas", ChatMessage.ImageInput.FromImageBytes(rawImage));
+        conversation.AppendSystemMessage(prompts.SystemPrompt);
+        conversation.AppendUserInput(prompts.ImagePrompt, ChatMessage.ImageInput.FromImageBytes(rawImage));
 
-            chat.AppendUserInput("Essa imagem é algum comprovante fiscal? Responda com SIM ou NAO");
-            string isReceiptAnswer = await chat.GetResponseFromChatbotAsync();
+        response.IsReceipt = await GetResponseAsync(conversation, prompts.IsReceiptPrompt, new NonReceiptException(input.Id));
+        response.Total = await GetResponseAsync(conversation, prompts.TotalPrompt);
+        response.Category = await GetResponseAsync(conversation, prompts.CategoryPrompt);
+        response.Description = await GetResponseAsync(conversation, prompts.DescriptionPrompt);
 
-            chat.AppendUserInput("Qual o valor total dessa despesa? Escreva o valor apenas com números.");
-            string totalAnswer = await chat.GetResponseFromChatbotAsync();
+        return response;
+    }
 
-            chat.AppendUserInput("Que categoria de despesa é essa? Responda entre: HOSPEDAGEM OU TRANSPORTE OU VIAGEM OU ALIMENTAÇÃO OU OUTROS.");
-            string categoryAnswer = await chat.GetResponseFromChatbotAsync();
+    public async Task<string> GetResponseAsync(Conversation conversation, string prompt, Exception? exceptionIfInvalid = null)
+    {
+        conversation.AppendUserInput(prompt);
+        var answer = await conversation.GetResponseFromChatbotAsync();
 
-            chat.AppendUserInput("Retorne uma descrição da nota fiscal em texto corrido (sem tópicos e apenas um parágrafo) apenas contendo: Nome da Empresa; CNPJ (caso possua); Data e horário da emissão da nota; Detalhamento dos itens");
-            string descriptionAnswer = await chat.GetResponseFromChatbotAsync();
+        if (exceptionIfInvalid != null && IsInvalidAnswer(answer))
+            throw exceptionIfInvalid;
 
-            //compor um prompt só para respostas, para que seja estritamente objetivo, separado por X, enviar lista, etc.
-            //passar um exemplo, etc. 
+        return answer;
+    }
 
-            var rawVisionReceipt = new RawVisionReceipt(
-                internalReceipt: input,
-                isReceipt: isReceiptAnswer,
-                total: totalAnswer,
-                category: categoryAnswer,
-                description: descriptionAnswer
-                );
+    public bool IsInvalidAnswer(string answer)
+    {
+        return Regex.IsMatch(answer, @"\bn(ã|a)o\b", RegexOptions.IgnoreCase);
+    }
 
-            var addedRawVisionReceipt = await _rawVisionReceiptRepository.AddAsync(rawVisionReceipt);
-            _logger.LogInformation($"Internal Receipt was interpreted by GPT Vision and added to repository (Id {addedRawVisionReceipt.Id})");
+    public async Task<RawVisionReceipt> CreateRawVisionReceiptAsync(InternalReceipt receipt, RawVisionResponse response)
+    {
+        var rawVisionReceipt = new RawVisionReceipt(receipt, isReceipt: response.IsReceipt, category: response.Category, total: response.Total, description: response.Description);
+        var addedRawVisionReceipt = await _rawVisionReceiptRepository.AddAsync(rawVisionReceipt);
+        _logger.LogInformation($"Internal Receipt was interpreted by GPT Vision and added to repository (Id: {addedRawVisionReceipt.Id})");
+        return addedRawVisionReceipt;
+    }
 
-            input.SetStatus(InternalReceiptStatusEnum.Successful);
-            var updateInternalReceipt = await _internalReceiptRepository.UpdateAsync(input);
-            _logger.LogInformation($"Internal Receipt status updated to {updateInternalReceipt.Status} (Id {updateInternalReceipt.Id})");
+    public async Task<InternalReceipt> UpdateInternalReceiptAsync(InternalReceipt input)
+    {
+        input.SetStatus(InternalReceiptStatusEnum.Successful);
+        var updateInternalReceipt = await _internalReceiptRepository.UpdateAsync(input);
+        _logger.LogInformation($"Internal Receipt status updated to {updateInternalReceipt.Status} (Id: {updateInternalReceipt.Id})");
+        return updateInternalReceipt;
+    }
 
-            return addedRawVisionReceipt;
-        }
+    public OpenAIAPI ConfigureApiKey()
+    {
+        var apiKey = _configuration[_visionConfig.OpenAIVisionApiEnvVar];
 
-        public OpenAIAPI ConfigureApiKey()
-        {
-            var envVariableApiKey = _configuration.GetSection("OpenAIVisionConfig:EnvVariable").Value;
+        if (string.IsNullOrWhiteSpace(apiKey))
+            throw new ApiKeyNotFoundException();
 
-            if (string.IsNullOrWhiteSpace(envVariableApiKey))
-                throw new ApiKeyNotFoundException();
+        return new OpenAIAPI(apiKey);
+    }
 
-            var APIKey = _configuration[envVariableApiKey];
-
-            if (string.IsNullOrWhiteSpace(APIKey))
-                throw new ApiKeyNotFoundException(envVariableApiKey);
-
-            return new OpenAIAPI(APIKey);
-        }
-
-        public bool IsExecutableStatus(InternalReceiptStatusEnum status)
-        {
-            return status != InternalReceiptStatusEnum.Successful
-                && status != InternalReceiptStatusEnum.Unsuccessful;
-        }
+    public bool IsExecutableStatus(InternalReceiptStatusEnum status)
+    {
+        return status != InternalReceiptStatusEnum.Successful
+            && status != InternalReceiptStatusEnum.Unsuccessful;
     }
 }
