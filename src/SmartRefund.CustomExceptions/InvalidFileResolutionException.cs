c24fbb79a0bbf3b53615fbef33ccaa08dc0c621b
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-16 11:39:00 -0300

    chore: exception adjustments.

---
+++ src/SmartRefund.CustomExceptions/InvalidFileResolutionException.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace SmartRefund.CustomExceptions
+{
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class InvalidFileResolutionException : Exception
+    {
+        public double RequiredPPI { get; private set; }
+        public double ImagePPI { get; private set; }
+        public InvalidFileResolutionException(double requiredPPI, double imagePPI)
+           : base($"Invalid file resolution, PPI should be >= {requiredPPI} (Image PPI: {imagePPI})")
+        {
+            RequiredPPI = requiredPPI;
+            ImagePPI = imagePPI;
+        }
+    }
+}
