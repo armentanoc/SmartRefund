a8eedbe42ef065a7689cb9720664613093e85ffc
Igor Nunes <inunes.us@gmail.com>
2024-03-18 06:14:13 -0300

    fix: merge error

--- src/SmartRefund.Infra/Migrations/20240317135348_CreateDatabaseInitial.Designer.cs
+++ src/SmartRefund.Infra/Migrations/20240317135348_CreateDatabaseInitial.Designer.cs
@@ -11,14 +11,9 @@ using SmartRefund.Infra.Context;
 namespace SmartRefund.Infra.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-<<<<<<<< HEAD:src/SmartRefund.Infra/Migrations/20240315205514_CreateDatabaseInitial.Designer.cs
-    [Migration("20240315205514_CreateDatabaseInitial")]
-    partial class CreateDatabaseInitial { 
-========
     [Migration("20240317135348_CreateDatabaseInitial")]
     partial class CreateDatabaseInitial
     {
->>>>>>>> ee66b419a05cde0ba95c94fda7ebc61c523ad7bb:src/SmartRefund.Infra/Migrations/20240317135348_CreateDatabaseInitial.Designer.cs
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -26,225 +21,225 @@ namespace SmartRefund.Infra.Migrations
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
 
             modelBuilder.Entity("SmartRefund.Domain.Models.Event", b =>
-                {
-                    b.Property<uint>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<uint>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Description")
-                        .HasColumnType("TEXT");
+                b.Property<string>("Description")
+                    .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("EventDate")
-                        .HasColumnType("TEXT");
+                b.Property<DateTime>("EventDate")
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("HashCode")
-                        .HasColumnType("TEXT");
+                b.Property<string>("HashCode")
+                    .HasColumnType("TEXT");
 
-                    b.Property<uint?>("ReceiptEventSourceId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint?>("ReceiptEventSourceId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("Status")
+                    .HasColumnType("INTEGER");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("ReceiptEventSourceId");
+                b.HasIndex("ReceiptEventSourceId");
 
-                    b.ToTable("Events");
-                });
+                b.ToTable("Events");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.InternalReceipt", b =>
-                {
-                    b.Property<uint>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<uint>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<DateTime>("CreationDate")
-                        .HasColumnType("TEXT");
+                b.Property<DateTime>("CreationDate")
+                    .HasColumnType("TEXT");
 
-                    b.Property<uint>("EmployeeId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint>("EmployeeId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<byte[]>("Image")
-                        .IsRequired()
-                        .HasColumnType("BLOB");
+                b.Property<byte[]>("Image")
+                    .IsRequired()
+                    .HasColumnType("BLOB");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("Status")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("UniqueHash")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("UniqueHash")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("InternalReceipt");
-                });
+                b.ToTable("InternalReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.RawVisionReceipt", b =>
-                {
-                    b.Property<uint>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<uint>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Category")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Category")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<uint>("InternalReceiptId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint>("InternalReceiptId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("IsReceipt")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("IsReceipt")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<bool>("IsTranslated")
-                        .HasColumnType("INTEGER");
+                b.Property<bool>("IsTranslated")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Total")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Total")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("UniqueHash")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("UniqueHash")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("InternalReceiptId");
+                b.HasIndex("InternalReceiptId");
 
-                    b.ToTable("RawVisionReceipt");
-                });
+                b.ToTable("RawVisionReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.ReceiptEventSource", b =>
-                {
-                    b.Property<uint>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<uint>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<int>("CurrentStatus")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("CurrentStatus")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<uint>("InternalReceiptId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint>("InternalReceiptId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<uint?>("RawVisionReceiptId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint?>("RawVisionReceiptId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<uint?>("TranslatedVisionReceiptId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint?>("TranslatedVisionReceiptId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("UniqueHash")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("UniqueHash")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("InternalReceiptId");
+                b.HasIndex("InternalReceiptId");
 
-                    b.HasIndex("RawVisionReceiptId");
+                b.HasIndex("RawVisionReceiptId");
 
-                    b.HasIndex("TranslatedVisionReceiptId");
+                b.HasIndex("TranslatedVisionReceiptId");
 
-                    b.ToTable("ReceiptEventSource");
-                });
+                b.ToTable("ReceiptEventSource");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.TranslatedVisionReceipt", b =>
-                {
-                    b.Property<uint>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+            {
+                b.Property<uint>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
 
-                    b.Property<int>("Category")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("Category")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<string>("Description")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("Description")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.Property<bool>("IsReceipt")
-                        .HasColumnType("INTEGER");
+                b.Property<bool>("IsReceipt")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<uint>("RawVisionReceiptId")
-                        .HasColumnType("INTEGER");
+                b.Property<uint>("RawVisionReceiptId")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
+                b.Property<int>("Status")
+                    .HasColumnType("INTEGER");
 
-                    b.Property<decimal>("Total")
-                        .HasColumnType("TEXT");
+                b.Property<decimal>("Total")
+                    .HasColumnType("TEXT");
 
-                    b.Property<string>("UniqueHash")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+                b.Property<string>("UniqueHash")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.HasIndex("RawVisionReceiptId");
+                b.HasIndex("RawVisionReceiptId");
 
-                    b.ToTable("TranslatedVisionReceipt");
-                });
+                b.ToTable("TranslatedVisionReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.Event", b =>
-                {
-                    b.HasOne("SmartRefund.Domain.Models.ReceiptEventSource", null)
-                        .WithMany("Events")
-                        .HasForeignKey("ReceiptEventSourceId");
-                });
+            {
+                b.HasOne("SmartRefund.Domain.Models.ReceiptEventSource", null)
+                    .WithMany("Events")
+                    .HasForeignKey("ReceiptEventSourceId");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.RawVisionReceipt", b =>
-                {
-                    b.HasOne("SmartRefund.Domain.Models.InternalReceipt", "InternalReceipt")
-                        .WithMany()
-                        .HasForeignKey("InternalReceiptId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.HasOne("SmartRefund.Domain.Models.InternalReceipt", "InternalReceipt")
+                    .WithMany()
+                    .HasForeignKey("InternalReceiptId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
 
-                    b.Navigation("InternalReceipt");
-                });
+                b.Navigation("InternalReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.ReceiptEventSource", b =>
-                {
-                    b.HasOne("SmartRefund.Domain.Models.InternalReceipt", "InternalReceipt")
-                        .WithMany()
-                        .HasForeignKey("InternalReceiptId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.HasOne("SmartRefund.Domain.Models.InternalReceipt", "InternalReceipt")
+                    .WithMany()
+                    .HasForeignKey("InternalReceiptId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
 
-                    b.HasOne("SmartRefund.Domain.Models.RawVisionReceipt", "RawVisionReceipt")
-                        .WithMany()
-                        .HasForeignKey("RawVisionReceiptId");
+                b.HasOne("SmartRefund.Domain.Models.RawVisionReceipt", "RawVisionReceipt")
+                    .WithMany()
+                    .HasForeignKey("RawVisionReceiptId");
 
-                    b.HasOne("SmartRefund.Domain.Models.TranslatedVisionReceipt", "TranslatedVisionReceipt")
-                        .WithMany()
-                        .HasForeignKey("TranslatedVisionReceiptId");
+                b.HasOne("SmartRefund.Domain.Models.TranslatedVisionReceipt", "TranslatedVisionReceipt")
+                    .WithMany()
+                    .HasForeignKey("TranslatedVisionReceiptId");
 
-                    b.Navigation("InternalReceipt");
+                b.Navigation("InternalReceipt");
 
-                    b.Navigation("RawVisionReceipt");
+                b.Navigation("RawVisionReceipt");
 
-                    b.Navigation("TranslatedVisionReceipt");
-                });
+                b.Navigation("TranslatedVisionReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.TranslatedVisionReceipt", b =>
-                {
-                    b.HasOne("SmartRefund.Domain.Models.RawVisionReceipt", "RawVisionReceipt")
-                        .WithMany()
-                        .HasForeignKey("RawVisionReceiptId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+            {
+                b.HasOne("SmartRefund.Domain.Models.RawVisionReceipt", "RawVisionReceipt")
+                    .WithMany()
+                    .HasForeignKey("RawVisionReceiptId")
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .IsRequired();
 
-                    b.Navigation("RawVisionReceipt");
-                });
+                b.Navigation("RawVisionReceipt");
+            });
 
             modelBuilder.Entity("SmartRefund.Domain.Models.ReceiptEventSource", b =>
-                {
-                    b.Navigation("Events");
-                });
+            {
+                b.Navigation("Events");
+            });
 #pragma warning restore 612, 618
         }
     }
