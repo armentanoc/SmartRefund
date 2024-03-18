818b1abdc2ceac1443f3f155f734d5f477dc2d94
czambanini <ca.zambanini@gmail.com>
2024-03-18 08:39:12 -0300

    criação da EventResponse para o endpoint AuditEventSouce

--- src/SmartRefund.ViewModels/Responses/AuditReceiptEventSourceResponse.cs
+++ src/SmartRefund.ViewModels/Responses/AuditReceiptEventSourceResponse.cs
@@ -11,13 +11,17 @@ namespace SmartRefund.ViewModels.Responses
     {
         public string UniqueHash { get; private set; }
         public string CurrentStatus { get; private set; }
-        public List<Event> Events { get; private set; }
+        public List<EventResponse> Events { get; private set; }
 
         public AuditReceiptEventSourceResponse(ReceiptEventSource receiptEventSource)
         {
             UniqueHash = receiptEventSource.UniqueHash;
             CurrentStatus = receiptEventSource.CurrentStatus.ToString();
-            Events = receiptEventSource.Events;
+            Events = new List<EventResponse>();
+            foreach (Event evnt in receiptEventSource.Events)
+            {
+                Events.Add(new EventResponse(evnt));
+            }
         }
 
     }
